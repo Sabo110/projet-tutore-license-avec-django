0b05506393fdfac6ef701417881e9395520e2c7a
sabo110 <samirphanuel@gmail.com>
2024-01-01 11:49:54 +0100

    font end de la home page

--- tutorial_project/tutorial_project/settings.py
+++ tutorial_project/tutorial_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-68nel@v=6t=(c-bljvv(14=(ld@c0v6lf1$prc%&m4*y*h*-v4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.43.192', '127.0.0.1']
 
 
 # Application definition
