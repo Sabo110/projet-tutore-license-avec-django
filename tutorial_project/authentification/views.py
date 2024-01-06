2c8defb20859eb6c5342176a29d11387a68f7d7c
sabo110 <samirphanuel@gmail.com>
2024-01-06 08:20:45 +0100

    mis ajour des liens de la navbar

--- tutorial_project/authentification/views.py
+++ tutorial_project/authentification/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.contrib.auth import logout
+
 from django.views import generic
 
 from .models import CustomUser
 from .forms import SignUpForm
 
+# la classe qui gere l'inscription des utilisateurs
 class SignUp(generic.CreateView):
-    model = CustomUser
-    form_class = SignUpForm
-    template_name = 'authentification/sign_up.html'
+    model = CustomUser # le model de creation d'instance d'utilisateur
+    form_class = SignUpForm # le formulaire d'inscription
+    template_name = 'authentification/sign_up.html' # le template sur lequel le formulaire sera rendu
+    success_url = reverse_lazy('login') # en cas de succes l'utilisateur sera dirige vers la page de login
+    
 
--- tutorial_project/tutorial_project/settings.py
+++ tutorial_project/tutorial_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from django.urls import reverse_lazy
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -129,3 +130,7 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "authentification.CustomUser"
+
+LOGIN_REDIRECT_URL = reverse_lazy('home_page')
+
+LOGOUT_REDIRECT_URL = reverse_lazy('home_page')
