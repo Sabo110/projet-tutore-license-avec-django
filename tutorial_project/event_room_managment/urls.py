5c92fdea1704cb2223e36516993917050566593c
ARAFAT RAHMAN10 <arafatmoluh@gmail.com>
2024-01-07 12:38:54 +0100

    Add Detail_Page

--- tutorial_project/event_room_managment/urls.py
+++ tutorial_project/event_room_managment/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.home_page, name='home_page'),
     path('members/', views.member_page, name='member_page'),
     path('profile/', views.profile, name='profile'),
+    path('detail/', views.detail, name='detail'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # l'ajout de ce parametre (+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)) permet l'affichage en front des fichiers statique comme les iamges
