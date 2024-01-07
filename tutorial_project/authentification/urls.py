dfd64e32852df7b8400474febac375ccd13e447d
sabo110 <samirphanuel@gmail.com>
2024-01-07 12:56:28 +0100

    ajout de commentaires

--- tutorial_project/authentification/urls.py
+++ tutorial_project/authentification/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('sign_up/', views.SignUp.as_view(), name='sign_up'),
-    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("django.contrib.auth.urls")), # j'inclus l'application fournit par django pour gerer l'authentification(login, logout)
     
 ]
